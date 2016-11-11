98c5c5308d29746539005aab1d282c716df96416
Mac <574989766@qq.com>
2016-11-11 10:06:49 +0800

    建商品详情controller

---
+++ ZeroLifeClient/ViewController/Home/ZLSuperMarket/ZLSuperMarketShop/ZLGoodsDetailViewController/ZLGoodsDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZLGoodsDetailViewController.h
+//  ZeroLifeClient
+//
+//  Created by Mac on 2016/11/11.
+//  Copyright © 2016年 瞿伦平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZLGoodsDetailViewController : UIViewController
+
+@end
---
+++ ZeroLifeClient/ViewController/Home/ZLSuperMarket/ZLSuperMarketShop/ZLGoodsDetailViewController/ZLGoodsDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  ZLGoodsDetailViewController.m
+//  ZeroLifeClient
+//
+//  Created by Mac on 2016/11/11.
+//  Copyright © 2016年 瞿伦平. All rights reserved.
+//
+
+#import "ZLGoodsDetailViewController.h"
+
+@interface ZLGoodsDetailViewController ()
+
+@end
+
+@implementation ZLGoodsDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
